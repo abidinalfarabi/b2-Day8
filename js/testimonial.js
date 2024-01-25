610e23a81db24e236119ed31acea94dea73fd71f
abidinalrabi <abidinalrabi2223@gmail.com>
2024-01-25 08:28:13 +0700

    tugas ke 8 day 8

--- js/testimonial.js
+++ js/testimonial.js
@@ -34,13 +34,20 @@ function html(item) {
 // function untuk menampilkan data sesuai banyak data yang ada pada array of object tadi yaitu Testimonial
 // yang menggunakan function forEach untuk melooping sesuai banyak data pada array of object tadi,
 // lalu menambahkan data ke file html menggunakn innerHTML
+//Higher-order function atau fungsi orde tinggi adalah fungsi yang
+//1) menerima fungsi lain sebagai parameternya, dan/atau
+//2) mengembalikan fungsi lain sebagai keluarannya.
+
 function testimonialData() {
   let testimonialHTML = ``;
   Testimonial.forEach((item) => {
     testimonialHTML += html(item);
   });
+
   document.getElementById("testimonials").innerHTML = testimonialHTML;
 }
+//forEach merupakan sebuah fungsi yang memungkinkan kita untuk melakukan sesuatu terhadap tiap-tiap elemen pada array.
+// Kita dapat menspesifikasikan apa yang dilakukan dengan memberikan fungsinya.
 
 // untuk menampilkan data ketika web baru dibuka
 testimonialData();
@@ -48,6 +55,9 @@ testimonialData();
 // function untuk membuat filter tentang rating dimana ketika kita memilih button sesuai dengan ratingnya
 // maka akan menampilkan isi data sesuai dengan rating yang kita pilih,
 // menggunakan function filter untuk mengembalikan value sesuai apa yang diminta
+//Callback pada dasarnya adalah sebuah function
+//Dengan kata lain function callback akan di berlakukan sebagai value di dalam function lain
+// sehingga function callback akan di ekseskusi setelah function yang membungkus function callback tersebut selesai di eksekusi.
 function filterTestimonials(Rating) {
   let testimonialHTML = ``;
   const testimonialFiltered = Testimonial.filter((item) => {
